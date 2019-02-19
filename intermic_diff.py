55a3727997a78a3ad738bed0ca44c3298df74099
Abilmansur Zhumabekov <abilmansur.999@gmail.com>
2019-02-19 11:58:45 +0900

    made useful plots

--- intermic_diff.py
+++ intermic_diff.py
@@ -5,7 +5,7 @@ import scipy.io as sio
 import matplotlib.pyplot as plt
 from scipy import stats
 
-def intermic_mag_diff(stft_magn):
+def intermic_mag_diff(stft_magn, plot_enable=False):
     # input : torch tensor of shape (num batches, F, nCH, T) for stft magnitude
     # outputs :
     #           torch tensor for log(Mi/M0) (or IMD), for i = ids of mics other than 0th
@@ -25,7 +25,11 @@ def intermic_mag_diff(stft_magn):
     mask = (stft_magn[:, :, 0, :] > mag_thr)  # NxFxT
     mask = mask.float()
     unmasked_freq_count = torch.sum(mask, dim=1)  # NxT
+
+    imd_nomask_allPairs = torch.zeros(N, F, nCombination, T)
+
     for i in range(nCH-1):
+        imd_nomask_allPairs[:,:,i,:] = stft_magn[:, :, i+1, :]/(stft_magn[:, :, 0, :] + eps)
         imd_masked = mask*stft_magn[:, :, i+1, :]/(stft_magn[:, :, 0, :] + eps)  # NxFxT
         imd_masked_mean = torch.sum(imd_masked, dim=1)/unmasked_freq_count  # NxT
         imd_masked_var = torch.sum(mask*(imd_masked-imd_masked_mean.expand_as(imd_masked))**2, dim=1)/unmasked_freq_count
@@ -40,11 +44,39 @@ def intermic_mag_diff(stft_magn):
     # output = torch.log(output+eps)
     # output = torch.clamp(output, min=minval, max=maxval)
 
+    if plot_enable:
+        # make some plots
+        imd_nomask_allPairs = imd_nomask_allPairs.numpy()
+        plt.close('all')
+        plt.figure()
+        plt.subplot(221)
+        plt.plot(imd_nomask_allPairs[0, :, 0, math.ceil(0.3 * T)])
+        plt.title('IMD at 0.3T')
+        plt.tight_layout()
+
+        plt.subplot(222)
+        plt.plot(imd_nomask_allPairs[0, :, 0, math.ceil(0.6 * T)])
+        plt.title('IMD at 0.6T')
+        plt.tight_layout()
+
+        plt.subplot(223)
+        plt.plot(stft_magn[0, :, 0, math.ceil(0.3 * T)].numpy())
+        plt.title('M reference at 0.3T')
+        plt.tight_layout()
+
+        plt.subplot(224)
+        plt.plot(stft_magn[0, :, 0, math.ceil(0.6 * T)].numpy())
+        plt.title('M reference at 0.6T')
+        plt.tight_layout()
+        plt.savefig('figures/IMD.png')
+        plt.show()
+
+
     # return output
     return imd_estimates, imd_confidences  # N x nCombination x T
 
 
-def intermic_phs_diff(stft_phs):
+def intermic_phs_diff(stft_phs, plot_enable=False):
     # input: torch tensor of shape (num batches, F, nCH, T) for stft phase
     # output: torch tensor for unwrapped phase differences, shape: (N, F, nCombination, T)
     PI = math.pi
@@ -52,31 +84,87 @@ def intermic_phs_diff(stft_phs):
     N, F, nCH, T = stft_phs.size()
     nCombination = nCH-1 # assume that reference mic appear at the first of dim
 
-    ipd = torch.FloatTensor(N, F, nCombination, T).zero_()
+    wrapped_ipd = torch.FloatTensor(N, F, nCombination, T).zero_()
 
     for i in range(nCH-1):
-        ipd[:, :, i, :] = stft_phs[:, :, i+1, :] - stft_phs[:, :, 0, :]
+        wrapped_ipd[:, :, i, :] = stft_phs[:, :, i+1, :] - stft_phs[:, :, 0, :]
 
     # wrap
-    ipd = torch.remainder(ipd + PI, TWOPI) - PI
+    wrapped_ipd = torch.remainder(wrapped_ipd + PI, TWOPI) - PI
 
     # unwrap
-    ipd = ipd.numpy()
-    ipd = np.unwrap(ipd, axis=1)
-    ipd = torch.FloatTensor(ipd)
+    wrapped_ipd = wrapped_ipd.numpy()
+    unwrapped_ipd = np.unwrap(wrapped_ipd, axis=1)
 
-    return ipd  # (N, F, nCombination, T)
+    if plot_enable:
+        # make some plots
+        plt.close('all')
+        plt.figure()
+        plt.subplot(221)
+        plt.plot(wrapped_ipd[0, :, 0, math.ceil(0.3*T)])
+        plt.title('wrapped ipd at 0.3T')
+        plt.tight_layout()
+
+        plt.subplot(223)
+        plt.plot(unwrapped_ipd[0,:,0,math.ceil(0.3*T)])
+        plt.title('unwrapped ipd at 0.3T')
+        plt.tight_layout()
+
+        plt.subplot(222)
+        plt.plot(wrapped_ipd[0, :, 0, math.ceil(0.6*T)])
+        plt.title('wrapped ipd at 0.6T')
+        plt.tight_layout()
+
+        plt.subplot(224)
+        plt.plot(unwrapped_ipd[0,:,0,math.ceil(0.6*T)])
+        plt.title('unwrapped ipd at 0.6T')
+        plt.tight_layout()
+        plt.savefig('figures/IPD.png')
+        plt.show()
+
+        plt.close('all')
+        plt.figure()
+        plt.subplot(211)
+        plt.plot(np.diff(wrapped_ipd[0, :, 0, math.ceil(0.3 * T)]))
+        plt.title('diff(ipd) at 0.3T')
+        plt.tight_layout()
+
+        plt.subplot(212)
+        plt.plot(np.diff(unwrapped_ipd[0, :, 0, math.ceil(0.6 * T)]))
+        plt.title('diff(ipd) at 0.6T')
+        plt.tight_layout()
+        plt.savefig('figures/diff(IPD).png')
+        plt.show()
+
+    unwrapped_ipd = torch.FloatTensor(unwrapped_ipd)
+    return unwrapped_ipd  # (N, F, nCombination, T)
 
 
-def intermic_tau_diff(stft_phs):
+def intermic_tau_diff(stft_phs, plot_enable=False):
     # input: torch tensor of shape (num batches, F, nCH, T) for stft phase
     # output: torch tensor for tau differences, and corresponding confidences
-    ipd = intermic_phs_diff(stft_phs)
+    ipd = intermic_phs_diff(stft_phs, plot_enable=plot_enable)  # N, F, nComb, T
     ipd = ipd.numpy()
-    diff_ipd = np.diff(ipd, axis=1)
-    slope_estimates = np.mean(diff_ipd, 1)
-    tau_diff_estimates = slope_estimates/(2*math.pi*8000/160)
-    tau_diff_confidences = 1/(np.std(diff_ipd, 1) + 1e-8)
+    diff_ipd = np.diff(ipd, axis=1)  # N, F, nComb, T
+    slope_estimates = np.mean(diff_ipd, 1)  # N, nComb, T
+    tau_diff_estimates = slope_estimates/(2*math.pi*8000/160)   # N, nComb, T
+    tau_diff_confidences = 1/(np.std(diff_ipd, 1) + 1e-8)   # N, nComb, T
+
+    if plot_enable:
+        # plot ipd at highest confidence, for 1 pair
+        plt.close('all')
+        plt.figure()
+        plt.subplot(211)
+        plt.plot(ipd[0,:,0, np.argmax(tau_diff_confidences[0, 0, :])])
+        plt.title('IPD at argmax(Confidence(t))')
+
+        plt.subplot(212)
+        plt.plot(np.diff(ipd[0,:,0, np.argmax(tau_diff_confidences[0, 0, :])]))
+        plt.title('diff(IPD) at argmax(Confidence(t))')
+
+        plt.savefig('figures/IPD(argmax(Confidence(t)).png')
+        plt.show()
+
     return tau_diff_estimates, tau_diff_confidences  # (num batches, nCombination, T)
 
 
@@ -144,19 +232,21 @@ def intermic_tau_diff(stft_phs):
 
 if __name__ == '__main__':
     pair_id = 0  # 0..2
-    # t_relative = 0.35
+    t_relative = 0.35
 
     # load inputs
-    mag = sio.loadmat('stft/case1_mag_20ms.mat')['mag']  # nCH, F, T
-    phs = sio.loadmat('stft/case1_phs_20ms.mat')['phs']  # nCH, F, T
+    mix_id = '1919-142785-0009'
+    # mix_id = 'case1'
+    mag = sio.loadmat('stft/{}_mag_20ms.mat'.format(mix_id))['mag']  # nCH, F, T
+    phs = sio.loadmat('stft/{}_phs_20ms.mat'.format(mix_id))['phs']  # nCH, F, T
     mag = torch.FloatTensor(mag)
     phs = torch.FloatTensor(phs)
     # reshape into (N, F, nCH, T)
     mag = mag.permute(1, 0, 2).unsqueeze(0)
     phs = phs.permute(1, 0, 2).unsqueeze(0)
 
-    imd_estimates, imd_confidences = intermic_mag_diff(mag)
-    tau_diff_estimates, tau_diff_confidences = intermic_tau_diff(phs)
+    imd_estimates, imd_confidences = intermic_mag_diff(mag, plot_enable=True)
+    tau_diff_estimates, tau_diff_confidences = intermic_tau_diff(phs, plot_enable=True)
 
     imd_estimates = imd_estimates.numpy()
     imd_confidences = imd_confidences.numpy()
@@ -164,17 +254,35 @@ if __name__ == '__main__':
     _, F, _, T = np.shape(mag)
 
     plt.close('all')
+    # tau difference errors and confidences
+    plt.figure()
+    plt.subplot(211)
+    plt.plot(tau_diff_estimates[0, pair_id, :])
+    plt.title('tau difference for pair {}-1'.format(pair_id + 2))
+    plt.ylabel('delay difference [s]')
+    plt.tight_layout()
+    plt.subplot(212)
+    plt.plot(tau_diff_confidences[0, pair_id, :])
+    plt.title('tau confidence for pair {}-1'.format(pair_id + 2))
+    plt.xlabel('time frame')
+    plt.tight_layout()
+    plt.savefig('figures/TauDiffVsTime.png')
+    plt.show()
 
+    # IMD errors and confidences
+    plt.close('all')
     plt.figure()
     plt.subplot(211)
     plt.plot(imd_estimates[0, pair_id, :])
-    plt.title('IMD for pair {}-1'.format(pair_id+1))
+    plt.title('IMD for pair {}-1'.format(pair_id+2))
     plt.tight_layout()
     plt.subplot(212)
     plt.plot(imd_confidences[0, pair_id, :])
-    plt.title('Confidence(t) for pair {}-1'.format(pair_id+1))
+    plt.title('IMD confidence for pair {}-1'.format(pair_id+2))
+    plt.xlabel('time frame')
     plt.tight_layout()
 
+    plt.savefig('figures/IMDvsTime.png')
     plt.show()
 
     ## estimates for a single pair:
--- testAllSingleSrcTwoMic.py
+++ testAllSingleSrcTwoMic.py
@@ -3,9 +3,14 @@ import os
 import numpy as np, scipy.io as sio, torch, matplotlib.pyplot as plt
 
 
-def normalized_abs_loss(estimate, truth):
-    loss = np.abs(estimate/(truth+1e-10) - 1)
-    return loss
+def relative_err(estimate, truth):
+    err = np.abs(estimate/(truth+1e-10) - 1)
+    return err
+
+
+def absolute_err(estimate, truth):
+    err = np.abs(estimate - truth)
+    return err
 
 
 if __name__ == '__main__':
@@ -13,7 +18,9 @@ if __name__ == '__main__':
     h_dir = 'H'
     stft_dir = 'stft'
 
-    imd_losses, tau_diff_losses = [], []
+    imd_errors, tau_diff_errors = [], []
+    tau_diff_estimate_list, tau_diff_truth_list = [], []
+    imd_estimate_list, imd_truth_list = [], []
     # for each wav file in src_dir:
     for filename in os.listdir(src_dir):
         if filename.endswith(".wav"):
@@ -30,11 +37,13 @@ if __name__ == '__main__':
             imd_estimates, imd_confidences = intermic_mag_diff(mag)
             imd_estimates = imd_estimates.numpy()
             imd_confidences = imd_confidences.numpy()
-            imd_estimate = np.mean(imd_estimates[0, :, :], axis=1)
+            imd_estimate = np.asscalar(np.mean(imd_estimates[0, :, :], axis=1))
             # imd_estimate = imd_estimates[0, 0, np.argmax(imd_confidences[0, 0, :])]
+            imd_estimate_list.append(imd_estimate)
 
             tau_diff_estimates, tau_diff_confidences = intermic_tau_diff(phs)
-            tau_diff_estimate = tau_diff_estimates[0, 0, np.argmax(tau_diff_confidences[0,0,:])]
+            tau_diff_estimate = -tau_diff_estimates[0, 0, np.argmax(tau_diff_confidences[0,0,:])]
+            tau_diff_estimate_list.append(tau_diff_estimate)
 
             # find true values
             h_dict = sio.loadmat(os.path.join(h_dir, '{}.mat'.format(mix_id)))
@@ -42,26 +51,57 @@ if __name__ == '__main__':
             h12 = h_dict['h12']
 
             imd_true = np.log(np.max(h12)/np.max(h11))
+            imd_truth_list.append(imd_true)
             tau_diff_true = (np.argmax(h12)-np.argmax(h11))/16000
+            tau_diff_truth_list.append(tau_diff_true)
 
-            imd_loss = normalized_abs_loss(imd_estimate, imd_true)
-            tau_diff_loss = normalized_abs_loss(tau_diff_estimate, tau_diff_true)
-            imd_losses.append(imd_loss)
-            tau_diff_losses.append(tau_diff_loss)
+
+            imd_err = absolute_err(-imd_estimate, imd_true)
+            tau_diff_err = absolute_err(tau_diff_estimate, tau_diff_true)
+            imd_errors.append(imd_err)
+            tau_diff_errors.append(tau_diff_err)
+
+            print('mix_id: {}'.format(mix_id))
+            print('tau_diff_estimate: {:.5g}s, tau_diff_true: {:.5g}s, tau_diff_error: {:.5g}s'.
+                  format(tau_diff_estimate, tau_diff_true, tau_diff_err))
+            print('imd_estimate: {:.5g}, imd_true: {:.5g}, imd_error: {:.5g}\n'.
+                  format(imd_estimate, imd_true, imd_err))
 
     plt.close('all')
+    # plotting errors
+    plt.figure()
+    plt.subplot(212)
+    plt.plot(imd_errors)
+    plt.title('IMD errors')
+    plt.xlabel('sample id')
+    plt.ylabel('absolute error')
+    plt.tight_layout()
+
+    plt.subplot(211)
+    plt.plot(tau_diff_errors)
+    plt.plot(np.abs(tau_diff_truth_list))
+    plt.legend(['|errors|', '|true tau diffs|'])
+    plt.title('tau difference errors')
+    plt.xlabel('sample id')
+    plt.ylabel('time [s]')
+    plt.tight_layout()
+    plt.savefig('figures/errors_for_all.png')
+
+    # juxtaposing estimates with truths
     plt.figure()
     plt.subplot(211)
-    plt.plot(imd_losses)
-    plt.title('IMD losses')
+    plt.plot(tau_diff_estimate_list)
+    plt.plot(tau_diff_truth_list)
+    plt.legend(['estimates', 'ground truths'])
+    plt.ylabel('tau difference [s]')
     plt.xlabel('sample id')
-    plt.ylabel('normalized abs loss')
     plt.tight_layout()
 
     plt.subplot(212)
-    plt.plot(tau_diff_losses)
-    plt.title('tau difference losses')
-    plt.xlabel('sample id')
-    plt.ylabel('normalized abs loss')
+    plt.plot(imd_estimate_list)
+    plt.plot(imd_truth_list)
+    plt.legend(['estimates', 'ground truths'])
+    plt.ylabel('log(M1/M0)')
     plt.tight_layout()
+    plt.savefig('figures/value_comparison_for_all.png')
     plt.show()
